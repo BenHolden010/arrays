a09f0647ce769a60ecde385c732dab5a281ee835
Benjamin holden <benjamin.f.holden@outlook.com>
2023-03-01 16:32:59 -0700

    Add explanation with examples

--- arrays.js
+++ arrays.js
@@ -11,3 +11,5 @@ console.log(name);
 console.log(sn);
 // these are the team leaders and the true was removed from the last slot (4th)
 console.log(tl);
+
+// to demonstrate my understanding of index positions, i will write an explanation of the reasoning behind using pop and shift. shift will remove the index position 0 because it is first, pop will remove the last index position, in the case of the name variable, dan is removed from index position 3 with the shift command. in the case of the sn, 72 was femoved from index position 0.
